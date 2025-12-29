1494c65af019f5def19aa227f7227d3e5d7c6f43
hyunsoo <sgn06221@gmail.com>
2025-12-29 22:13:37 +0900

    chore: 폴더 명 변경

---
+++ 섹션 2/AA.py
@@ -0,0 +1,22 @@
+# import sys
+# sys.stdin = open("input.txt", "rt") 
+N = int(input())
+input_list = list(map(int, input().split()))
+
+sum_cnt = 0
+plus_cnt = 1
+
+for i in range(N):
+    if i == 0:
+        if input_list[i] == 1:
+            sum_cnt += 1
+    else:
+        if input_list[i] == input_list[i-1] == 1:
+            plus_cnt += 1
+            sum_cnt += plus_cnt
+
+        elif input_list[i] == 1 and input_list[i] != input_list[i-1]:
+            sum_cnt += 1
+            plus_cnt = 1
+
+print(sum_cnt)
