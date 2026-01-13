0ab39f749bf0e07af6ac7a355d3cd34621b3ce44
hyunsoo <sgn06221@gmail.com>
2026-01-13 20:55:08 +0900

    refactor: 쇠막대기

--- 섹션 5/02_steel_bar.py
+++ 섹션 5/02_steel_bar.py
@@ -7,11 +7,10 @@ for i in range(len(input_lst)):
     if input_lst[i] == '(':
         stack.append(i)
     else:
+        stack.pop()
         if input_lst[i-1] == '(':
-            stack.pop()
             cnt += len(stack)
         else:
-            stack.pop()
             cnt += 1
 
 print(cnt)
